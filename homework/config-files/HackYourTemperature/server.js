03a229a09c25b3509dc95cc41dc9caced0c7afb9
Fadi <fady-naddaf@hotmail.com>
2022-06-14 23:52:57 +0200

    Node homework week-1

---
+++ homework/config-files/HackYourTemperature/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import express from 'express';
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+
+app.post('/weather', (req, res) => {
+  const cityName = req.body.cityName;
+  if (!cityName) {
+    res.status(400).json({ msg: 'Please enter a valid city name' });
+  } else {
+    res.status(200).json({ cityName });
+  }
+});
+
+app.listen(PORT);
